ab294c30af957c13ecbc0f05d0f463bbe219a06a
Raus <rausaray1234@gmail.com>
2025-08-22 10:47:17 +0545

    fix: CI CD flow

--- src/components/form/DripForm.tsx
+++ src/components/form/DripForm.tsx
@@ -110,11 +110,6 @@ export default function DripFormModal({
       if (data.cover instanceof File) {
         formData.append("images", data.cover);
       }
-
-      formData.forEach((value, key) => {
-        console.log(key, value);
-      });
-
       let result: Product;
 
       if (initialData) {
